bb5a7528ea216b9aa5f98500e8c146e260448ad4
Yuriy Kornienko <dynamost@dynamost.ru>
2023-02-20 23:50:24 +0300

    fix errors and make test atomic

--- __tests__/todomvc.test.js
+++ __tests__/todomvc.test.js
@@ -2,15 +2,15 @@ import 'chromedriver'
 import { browser, have, perform } from 'selenidejs'
 import {test, afterAll} from '@jest/globals'
 
-test('add todos on TodoMVC page', async () => {
+test('add todos', async () => {
   await browser.open('http://todomvc.com/examples/emberjs/')
 
   await browser.element('#new-todo').type('a').then(perform.pressEnter)
   await browser.element('#new-todo').type('b').then(perform.pressEnter)
   await browser.element('#new-todo').type('c').then(perform.pressEnter)
-  await browser.all('#todo-list>li').should(have.exactTexts('a', 'b', 'c'))
-
-  await browser.element('#todo-list>li:nth-child(2) .toggle').click()
+  // await browser.all('#todo-list>li').should(have.exactTexts('a', 'b', 'c'))
+  await browser.element('#todo-list>li:nth-of-type(2) .toggle').click()
+  
   await browser.all('#todo-list>li.completed').should(have.exactTexts('b'))
   await browser.all('#todo-list>li:not(.completed)').should(have.exactTexts('a', 'c'))
 })
